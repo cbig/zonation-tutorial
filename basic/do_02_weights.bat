b55ce9d62385cdb3cd3ae32c7cd9c7c4849393a6
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2013-09-11 17:32:58 +0300

    Rename variants 01-02

---
+++ basic/do_02_weights.bat
@@ -0,0 +1,1 @@
+call zig3.exe -r 02_weights/02_weights.dat 02_weights/02_weights.spp basic_output/02_weights/output_02_weights.txt 0.0 0 1.0 0
--- basic/do_basic_01_caz.bat
+++
@@ -1,1 +0,0 @@
-call zig3.exe -r basic_01_caz/basic_01_caz.dat basic_01_caz/basic_01_caz.spp basic_output/basic_01_caz/basic_01_caz.txt 0.0 0 1.0 1
