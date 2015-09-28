201d35a07ee6dc5ddff498bbb537fa572d27f45c
Peter Kullberg <peter.kullberg@helsinki.fi>
2015-09-28 21:22:08 +0300

    Added vmat example: Variants 07_save_vmat and 08_load_vmat.

---
+++ advanced/do_07_save_vmat.bat
@@ -0,0 +1,1 @@
+call zig4.exe -r 07_save_vmat/07_save_vmat.dat 07_save_vmat/07_save_vmat.spp advanced_output/07_save_vmat/07_save_vmat.txt 0.0 0 1.0 1
---
+++ advanced/do_08_load_vmat.bat
@@ -0,0 +1,1 @@
+call zig4.exe -r 08_load_vmat/08_load_vmat.dat 08_load_vmat/08_load_vmat.spp advanced_output/08_load_vmat/08_load_vmat.txt 0.0 0 1.0 1
